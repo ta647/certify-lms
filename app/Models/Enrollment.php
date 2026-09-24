82e4b4394f4d1bcdc6969da787ddce7b202d1285
tateoka <tomboy0803@yahoo.com>
2026-09-24 15:33:00 +0900

    コーチの受講生一覧に担当外の受講生まで表示されるバグを修正

--- app/Models/Enrollment.php
+++ app/Models/Enrollment.php
@@ -204,7 +204,7 @@ class Enrollment extends Model
     {
         return match ($user->role) {
             UserRole::Admin => $query,
-            UserRole::Coach => $query,
+            UserRole::Coach => $query->whereIn('certification_id', $user->coachingCertificationIds()),
             UserRole::Student => $query->where('user_id', $user->id),
             default => $query->whereRaw('1 = 0'),
         };
